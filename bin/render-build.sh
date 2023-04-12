28033ee3cf69919ed0eb619fc30264ac4e85fa04
Njunu-sk <simonnjunu16@gmail.com>
2023-04-13 02:09:43 +0300

    minor

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,4 +3,8 @@
 set -o errexit
 
 bundle install
-bundle exec rake db:reset
+
+RAILS_ENV=production rake db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1 
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake db:seed
